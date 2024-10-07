efe49515a1ceee94ed6d147dfe2ca8375b26a5b3
ThimiraPathirana <36132763+ThimiraPathirana@users.noreply.github.com>
2024-10-07 23:29:38 +0530

    check dynamic value usage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+const title = 'Welcome to my new blog';
+const likes = 50;
+const link = 'http://google.com'
 
 function App() {
   return (
     <div className="App">
       <div className="content">
-        <h2>App Component</h2>
+        <h2>{title}</h2>
+        <h2>Likes {likes}</h2>
+
+        <p>10</p>
+        <p>'Hello thimira'</p>
+        <p>{[1,2,3,4]}</p>
+        <p>{Math.random() * 10}</p>
+        <a href={link}>Google site</a>
       </div>
     </div>
   );
